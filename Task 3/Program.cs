777abf6a4111d547a594a83579586cddca6bc211
Savely <Savka9765@yandex.ru>
2022-08-25 21:51:45 +0300

    Программа задает двумерный массив m x n

---
+++ Task 3/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Задайте двумерный массив размером m×n,
+заполненный случайными целыми числами.*/
+
+Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int lines = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = FillArray(lines, columns);
+PrintArray(array);
+
+int[,] FillArray(int lines, int columns)
+{
+    int[,] result = new int[lines, columns];
+    for (int i = 0; i < lines; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
