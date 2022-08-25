c43d880b2cf72cccf23fed890ce476e26d794c15
Savely <Savka9765@yandex.ru>
2022-08-25 21:52:29 +0300

    Программа задает двумерный массив и находит сумму элементов, находящихся по диагонали

---
+++ Task 4/Program.cs
@@ -0,0 +1,56 @@
+﻿/*Задайте двумерный массив. Найдите сумму
+элементов, находящихся на главной диагонали (с индексами
+(0,0); (1;1) и т.д.*/
+
+Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int lines = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = FillArray(lines, columns);
+PrintArray(array);
+Console.WriteLine();
+Console.Write($"Сумма элементов, находящихся на главной диагонали = {SumOfDiagonalElements(array)}");
+
+int[,] FillArray(int lines, int columns)
+{
+    int[,] result = new int[lines, columns];
+    for (int i = 0; i < lines; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return result;
+}
+
+int SumOfDiagonalElements(int[,] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                sum = sum + array[i,j];
+            }
+        }
+    }
+    return sum;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
