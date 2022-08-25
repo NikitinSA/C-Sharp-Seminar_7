6e2b8ed689467ceb6cb38768497358e6b2b293ca
Savely <Savka9765@yandex.ru>
2022-08-25 21:48:12 +0300

    Программа задает двумерный массив и находит эллементы, у которых оба индекса четные. Меняет эти эллементы на их квадраты.

---
+++ Task 1/Program.cs
@@ -0,0 +1,55 @@
+﻿/*Задайте случайный массив размером m на n.
+ Найдите эллементы у ктрых оба индекса четные и замените 
+ эти эллементы на их квадраты.*/
+
+Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int lines = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = FillArray(lines, columns);
+PrintArray(array);
+Console.WriteLine();
+PrintArray(ChangeArray(array));
+
+int[,] FillArray(int lines, int columns)
+{
+    int[,] result = new int[lines, columns];
+    for (int i = 0; i < lines; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return result;
+}
+
+int[,] ChangeArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                array[i, j] *= array[i, j];
+            }
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
