b4ca676025dc311fc9fef67a4f485fde0789ccc9
Savely <Savka9765@yandex.ru>
2022-08-25 21:51:17 +0300

    Программа задает двумерный массив по формуле m+n и выдает на экран

---
+++ Task 2/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Задайте двумерный массив размера m на n,
+каждый элемент в массиве находится по формуле: Aₙₙ = m+n.
+Выведите полученный массив на экран.
+m = 3, n = 4.
+0 1 2 3
+1 2 3 4
+2 3 4 5*/
+
+Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int lines = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = FillArray(lines, columns);
+PrintArray(array);
+
+int[,] FillArray(int lines, int columns)
+{
+    int[,] result = new int[lines, columns];
+    for (int i = 0; i < lines; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i,j] = i + j;
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
